c07dc8b116163dc32915da65ff71b50158854968
arafat-213 <tai.arafat.at@gmail.com>
2020-05-21 21:05:53 +0530

    added swipe up/down logic

--- src/App.js
+++ src/App.js
@@ -99,6 +99,94 @@ const App = () => {
 			setData(newArray)
 		}
 	}
+
+	const swipeDown = dummy => {
+		console.log(data)
+		let b = cloneDeep(data)
+		let oldData = JSON.parse(JSON.stringify(data))
+		for (let i = 3; i >= 0; i--) {
+			let slow = b.length - 1
+			let fast = slow - 1
+			while (slow > 0) {
+				if (fast === -1) {
+					fast = slow - 1
+					slow--
+					continue
+				}
+				if (b[slow][i] === 0 && b[fast][i] === 0) {
+					fast--
+				} else if (b[slow][i] === 0 && b[fast][i] !== 0) {
+					b[slow][i] = b[fast][i]
+					b[fast][i] = 0
+					fast--
+				} else if (b[slow][i] !== 0 && b[fast][i] === 0) {
+					fast--
+				} else if (b[slow][i] !== 0 && b[fast][i] !== 0) {
+					if (b[slow][i] === b[fast][i]) {
+						b[slow][i] = b[slow][i] + b[fast][i]
+						b[fast][i] = 0
+						fast = slow - 1
+						slow--
+					} else {
+						slow--
+						fast = slow - 1
+					}
+				}
+			}
+		}
+		if (JSON.stringify(b) !== JSON.stringify(oldData)) {
+			// addNumber(b)
+		}
+		if (dummy) {
+			return b
+		} else {
+			setData(b)
+		}
+	}
+
+	const swipeUp = dummy => {
+		let b = cloneDeep(data)
+		let oldData = JSON.parse(JSON.stringify(data))
+		for (let i = 0; i < 4; i++) {
+			let slow = 0
+			let fast = 1
+			while (slow < 4) {
+				if (fast === 4) {
+					fast = slow + 1
+					slow++
+					continue
+				}
+				if (b[slow][i] === 0 && b[fast][i] === 0) {
+					fast++
+				} else if (b[slow][i] === 0 && b[fast][i] !== 0) {
+					b[slow][i] = b[fast][i]
+					b[fast][i] = 0
+					fast++
+				} else if (b[slow][i] !== 0 && b[fast][i] === 0) {
+					fast++
+				} else if (b[slow][i] !== 0 && b[fast][i] !== 0) {
+					if (b[slow][i] === b[fast][i]) {
+						b[slow][i] = b[slow][i] + b[fast][i]
+						b[fast][i] = 0
+						fast = slow + 1
+						slow++
+					} else {
+						slow++
+						fast = slow + 1
+					}
+				}
+			}
+		}
+		if (JSON.stringify(oldData) !== JSON.stringify(b)) {
+			// addNumber(b)
+		}
+		if (dummy) {
+			return b
+		} else {
+			setData(b)
+		}
+	}
+
 	return (
 		<div
 			className='App'
