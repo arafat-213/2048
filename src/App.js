35790413f82b5b8d9eb3ea7673162b8ae9818ee2
arafat-213 <tai.arafat.at@gmail.com>
2020-05-21 21:04:53 +0530

    added swipe left/right logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import cloneDeep from 'lodash.clonedeep'
 
 const App = () => {
 	const [data, setData] = useState([
@@ -7,6 +8,97 @@ const App = () => {
 		[0, 0, 0, 0],
 		[0, 0, 0, 0]
 	])
+
+	// Swipe Left
+	const swipeLeft = dummy => {
+		let oldGrid = data
+		let newArray = cloneDeep(data)
+
+		for (let i = 0; i < 4; i++) {
+			let b = newArray[i]
+			let slow = 0
+			let fast = 1
+			while (slow < 4) {
+				if (fast === 4) {
+					fast = slow + 1
+					slow++
+					continue
+				}
+				if (b[slow] === 0 && b[fast] === 0) {
+					fast++
+				} else if (b[slow] === 0 && b[fast] !== 0) {
+					b[slow] = b[fast]
+					b[fast] = 0
+					fast++
+				} else if (b[slow] !== 0 && b[fast] === 0) {
+					fast++
+				} else if (b[slow] !== 0 && b[fast] !== 0) {
+					if (b[slow] === b[fast]) {
+						b[slow] = b[slow] + b[fast]
+						b[fast] = 0
+						fast = slow + 1
+						slow++
+					} else {
+						slow++
+						fast = slow + 1
+					}
+				}
+			}
+		}
+		if (JSON.stringify(oldGrid) !== JSON.stringify(newArray)) {
+			// addNumber(newArray);
+		}
+		if (dummy) {
+			return newArray
+		} else {
+			setData(newArray)
+		}
+	}
+
+	const swipeRight = dummy => {
+		let oldData = data
+		let newArray = cloneDeep(data)
+
+		for (let i = 3; i >= 0; i--) {
+			let b = newArray[i]
+			let slow = b.length - 1
+			let fast = slow - 1
+			while (slow > 0) {
+				if (fast === -1) {
+					fast = slow - 1
+					slow--
+					continue
+				}
+				if (b[slow] === 0 && b[fast] === 0) {
+					fast--
+				} else if (b[slow] === 0 && b[fast] !== 0) {
+					b[slow] = b[fast]
+					b[fast] = 0
+					fast--
+				} else if (b[slow] !== 0 && b[fast] === 0) {
+					fast--
+				} else if (b[slow] !== 0 && b[fast] !== 0) {
+					if (b[slow] === b[fast]) {
+						b[slow] = b[slow] + b[fast]
+						b[fast] = 0
+						fast = slow - 1
+						slow--
+					} else {
+						slow--
+						fast = slow - 1
+					}
+				}
+			}
+		}
+		if (JSON.stringify(newArray) !== JSON.stringify(oldData)) {
+			// addNumber(newArray)
+		}
+		if (dummy) {
+			return newArray
+		} else {
+			setData(newArray)
+		}
+	}
 	return (
 		<div
 			className='App'
