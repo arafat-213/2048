806ab18fee85dd429121a1a209d7c82056007df9
arafat-213 <tai.arafat.at@gmail.com>
2020-05-21 22:38:20 +0530

    Updates

---
+++ precache-manifest.b5723b4ecb5ce21107a03d940537962d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edd8d98491cc59286fd1079257d17f4f",
+    "url": "/2048/index.html"
+  },
+  {
+    "revision": "f2d3c3044e9e0011fb54",
+    "url": "/2048/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "803340ee52930b9b5a66",
+    "url": "/2048/static/js/2.85d3601a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2048/static/js/2.85d3601a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2d3c3044e9e0011fb54",
+    "url": "/2048/static/js/main.db8cab20.chunk.js"
+  },
+  {
+    "revision": "4440c455c45c178fe285",
+    "url": "/2048/static/js/runtime-main.8060863c.js"
+  }
+]);
