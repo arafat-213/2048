d676b408087d73c84601a91a55cb4d48530069b2
arafat-213 <tai.arafat.at@gmail.com>
2020-05-21 21:12:01 +0530

    added styles

--- src/App.js
+++ src/App.js
@@ -329,7 +329,7 @@ const style = {
 	blockStyle: {
 		height: 80,
 		width: 80,
-		background: 'ligthgray',
+		background: 'lightgray',
 		margin: 3,
 		display: 'flex',
 		justifyContent: 'center',
@@ -337,6 +337,40 @@ const style = {
 		fontSize: 45,
 		fontWeight: '800',
 		color: 'white'
+	},
+	newGameButton: {
+		padding: 10,
+		background: '#846F5B',
+		color: '#F8F5F0',
+		width: 95,
+		borderRadius: 7,
+		fontWeight: '900',
+		marginLeft: 'auto',
+		marginBottom: 'auto',
+		cursor: 'pointer'
+	},
+	tryAgainButton: {
+		padding: 10,
+		background: '#846F5B',
+		color: '#F8F5F0',
+		width: 80,
+		borderRadius: 7,
+		fontWeight: '900',
+		cursor: 'pointer',
+		margin: 'auto',
+		marginTop: 20
+	},
+	gameOverOverlay: {
+		position: 'absolute',
+		height: '100%',
+		width: '100%',
+		left: 0,
+		top: 0,
+		borderRadius: 5,
+		background: 'rgba(238,228,218,.5)',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center'
 	}
 }
 
